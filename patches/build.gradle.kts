ff545bc5a390056f88d1a8d77eb12b1dfc8e163f
5punk <avaneesh@2die4.com>
2025-03-17 19:13:32 -0700

    feat: ifunny noads, premium, save video patch

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -48,10 +48,10 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/revanced-patches")
+            url = uri("https://maven.pkg.github.com/5punk/revanced-patches")
             credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+                username = ""
+                password = ""
             }
         }
     }
